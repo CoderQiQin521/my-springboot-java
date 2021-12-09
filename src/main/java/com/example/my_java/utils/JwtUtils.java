646525708a52d6f3efd2a9066968fecd28ac2b13
齐秦 <coderqiqin@aliyun.com>
2021-12-09 18:37:35 +0800

    jwt token

--- src/main/java/com/example/my_java/utils/JwtUtils.java
+++ src/main/java/com/example/my_java/utils/JwtUtils.java
@@ -1,14 +1,28 @@
 package com.example.my_java.utils;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
+import java.util.Base64;
 import java.util.Calendar;
 import java.util.Date;
 
 @MyAnnotation(info = "注解执行了")
 public class JwtUtils {
 //    @Deprecated
+
+    /**
+     * 签发时间: 现在
+     * 过期时间30分钟
+     * 载荷内容: 用户名
+     * 加密字符串 hello
+     *
+     * @param userId   签发对象这个用户的id
+     * @param realname
+     * @return
+     */
     public static String createToken(String userId, String realname) {
 
         Calendar nowTime = Calendar.getInstance();
@@ -19,6 +33,12 @@ public class JwtUtils {
                 .withIssuedAt(new Date())
                 .withExpiresAt(expiresDate)
                 .withClaim("realname", realname)
-                .sign(Algorithm.HMAC256(userId+"hello"));
+                .sign(Algorithm.HMAC256(userId + "hello"));
+    }
+
+    public static DecodedJWT verifyToken(String token) throws Exception {
+        DecodedJWT jwt = null;
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(Base64.getDecoder().decode("1hello"))).build();
+        return verifier.verify(token);
     }
 }
--- src/test/java/com/example/my_java/utils/JwtTest.java
+++ src/test/java/com/example/my_java/utils/JwtTest.java
@@ -1,5 +1,6 @@
 package com.example.my_java.utils;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,5 +11,17 @@ public class JwtTest {
     public void test() {
         String token = JwtUtils.createToken("1", "齐秦");
         System.out.println(token);
+    }
+
+    @Test
+    public void ver() {
+        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxODUzODMwMDgzOSIsImV4cCI6MTYzOTA0NTM1MywiaWF0IjoxNjM5MDQzNTUzLCJyZWFsbmFtZSI6InFpcWluIn0.dnoXT2NPfy1RgzDOd1tm2PLWHoyizyLxYRn8BHqo7Lo";
+
+        try {
+            DecodedJWT aa = JwtUtils.verifyToken(token);
+            System.out.println(aa);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
